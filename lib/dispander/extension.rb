e202dde64455180b86ed579a84d06217590a5569
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-01 18:42:49 +0900

    Update: Improve sending

--- lib/dispander/extension.rb
+++ lib/dispander/extension.rb
@@ -18,27 +18,42 @@ module Dispander
     #
     # メッセージを解析して、埋め込みを送信します。
     #
-    # @param [Discorb::Message] message 解析するメッセージ。
+    # @param [Discorb::Message] base_message 解析するメッセージ。
     #
     # @return [Array<Discorb::Message>] 埋め込みを送信したメッセージ。
     #
-    def dispand(message)
-      sent_messages = []
-      message.content.scan(DISCORD_URL_PATTERN).each do |match|
+    def dispand(base_message)
+      all_sent_messages = []
+      base_message.content.scan(DISCORD_URL_PATTERN).each do |match|
         guild_id, channel_id, message_id = *match
-        next unless message.channel.guild.id == guild_id
+        next unless base_message.guild.id == guild_id
 
         embeds = []
-        message = @client.guilds[guild_id].channels[channel_id].fetch_message(message_id).wait
-        embed = create_embed_from_message(message)
-        embeds << embed
-        embeds += message.embeds
-        embeds += message.attachments.map { |attachment| create_embed_from_attachment(attachment) }
 
-        until (embeds_send = embeds.slice!(..10)).empty?
-          sent_messages << message.channel.post(embeds: embeds_send).wait
+        sent_messages = []
+        begin
+          message = @client.guilds[guild_id].channels[channel_id].fetch_message(message_id).wait
+        rescue Discorb::NotFoundError
+          next
+        else
+          embed = create_embed_from_message(message)
+          embeds << embed
+          embeds += message.embeds
+          embeds += message.attachments[1..]&.filter(&:image?)&.map { |attachment| create_embed_from_attachment(attachment) }
+
+          until (embeds_send = embeds.slice!(..10)).empty?
+            # @type [Array<Discorb::Message>]
+            sent_messages << message.channel.post(embeds: embeds_send).wait
+          end
+          embed.url = "http://a.io/#{base_message.author.id}-#{message.author.id}-#{sent_messages.map(&:id).join(",")}"
+          first_embeds = sent_messages[0].embeds
+          first_embeds[0] = embed
+          sent_messages[0].add_reaction(Discorb::UnicodeEmoji["wastebasket"])
+          sent_messages[0].edit(embeds: first_embeds).wait
+          all_sent_messages += sent_messages
         end
       end
+      all_sent_messages
     end
 
     #
@@ -59,6 +74,9 @@ module Dispander
       embed.footer = Discorb::Embed::Footer.new(
         "#" + message.channel.name
       )
+      if (attachment = message.attachments[0]) && attachment.image?
+        embed.image = attachment.proxy_url
+      end
       embed
     end
 
@@ -70,15 +88,11 @@ module Dispander
     # @return [Discorb::Embed] 埋め込み。
     #
     def create_embed_from_attachment(attachment)
-      embed = Discorb::Embed.new
-      if attachment.image?
-        embed.image = Discorb::Embed::Image.new(
-          attachment.url,
-        )
-      else
-        embed.description = "[添付ファイル：#{attachment.filename}](#{attachment.url})"
-      end
+      embed = Discorb::Embed.new(
+        image: attachment.proxy_url,
+      )
       embed
+    end
 
     def delete_message(event)
       return unless event.emoji.name == "wastebasket"
